4f3ab7c4012385f098935b3dc5a653ff8b1e80dd
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-06-05 22:46:02 +0100

    Header component Ui

---
+++ frontend/src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className="CTA">
+        {/* <a href="">CONTACT US</a> */}
+        <a href="#contact" className='btn'>CONTACT US</a>
+    </div>
+  )
+}
+
+export default CTA
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import COVER from '../../images/cover.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className="conatiner header_container">
+          <h5>Hello</h5>
+          <h1>Salma Jadwa</h1>
+          <h5 className="text-light"> chi tkharbi9aaaaaaaa</h5>
+          <CTA />
+
+          <div className="cover">
+            <img src={COVER} alt="" />
+          </div>
+
+          {/* <a href="#contact" className='scroll__down'>Scroll Down</a> */}
+        </div>
+    </header>
   )
 }
 
