e8b43be69f7a8772ac43204bfcbed1393fe72493
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-06-06 15:40:26 +0100

    Styling About Us section

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -20,36 +20,32 @@ const About = () => {
           <div className="about__cards">
             <article className="about__card">
               <FiUsers className='about__icon'/>
-              <h5>Experience</h5  >
-              <small>3+ Years working</small>
+              <h5>17</h5  >
+              <small>Orpholinat</small>
              </article>
 
              <article className="about__card">
               <FiUsers className='about__icon'/>
-              <h5>Experience</h5  >
-              <small>3+ Years working</small>
+              <h5>172</h5  >
+              <small>Enfants</small>
              </article>
 
              <article className="about__card">
               <FiUsers className='about__icon'/>
-              <h5>Experience</h5  >
-              <small>3+ Years working</small>
+              <h5>73</h5  >
+              <small>Adopté(e)</small>
              </article>
 
              <article className="about__card">
               <FiUsers className='about__icon'/>
-              <h5>Experience</h5  >
-              <small>3+ Years working</small>
+              <h5>61</h5  >
+              <small>Hébergé(e)</small>
              </article>
           </div>
 
           <p>
-            C'est un fait établi depuis longtemps qu'un lecteur sera distrait par le contenu lisible 
-            d'une page en regardant sa mise en page. L'intérêt d'utiliser le Lorem Ipsum est qu'il présente 
-            une distribution plus ou moins normale des lettres, par opposition à l'utilisation de "Contenu ici, contenu ici", 
-            ce qui lui donne l'apparence d'un anglais lisible. De nombreux logiciels de publication assistée par ordinateur 
-            et éditeurs de pages Web utilisent désormais le Lorem Ipsum comme modèle de texte par défaut, et une recherche sur
-            "lorem ipsum" permet de découvrir de nombreux sites Web qui en sont encore à leurs débuts.
+            C'est un fait établi depuis longtemps qu'un lecteur sera distrait par le contenu lisible d'une page en regardant sa mise en page. L'intérêt d'utiliser le Lorem Ipsum est qu'il présente 
+            une distribution plus ou moins normale des lettres, par opposition à l'utilisation de "Contenu ici, contenu ici", ce qui lui donne l'apparence d'un anglais lisible. De nombreux logiciels de publication assistée par ordinateur et éditeurs de pages Web utilisent désormais le Lorem Ipsum comme modèle de texte par défaut, et une recherche sur"lorem ipsum" permet de découvrir de nombreux sites Web qui en sont encore à leurs débuts.
           </p>
 
           <a href="#conatct" className='btn btn-primary'>Découvrir nous orpholinats</a>
