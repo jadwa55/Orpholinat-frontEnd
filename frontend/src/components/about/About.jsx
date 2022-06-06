6abc672b829cbc9a151006e5b3a44a6d254f1ee0
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-06-06 13:58:10 +0100

    About Us section

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
 import './about.css'
-import img from '../../images/girl/png'
+import img from '../../images/girl.png'
+import {FiUsers} from 'react-icons/fi'
 
 const About = () => {
   return (
     <section id='about'>
-      about
+      <h5>A propos de nous </h5>
+      <h2>Qui somme nous?</h2>
+
+      <div className=".container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={img} alt="" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5  >
+              <small>3+ Years working</small>
+             </article>
+
+             <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5  >
+              <small>3+ Years working</small>
+             </article>
+
+             <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5  >
+              <small>3+ Years working</small>
+             </article>
+
+             <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5  >
+              <small>3+ Years working</small>
+             </article>
+          </div>
+
+          <p>
+            C'est un fait établi depuis longtemps qu'un lecteur sera distrait par le contenu lisible 
+            d'une page en regardant sa mise en page. L'intérêt d'utiliser le Lorem Ipsum est qu'il présente 
+            une distribution plus ou moins normale des lettres, par opposition à l'utilisation de "Contenu ici, contenu ici", 
+            ce qui lui donne l'apparence d'un anglais lisible. De nombreux logiciels de publication assistée par ordinateur 
+            et éditeurs de pages Web utilisent désormais le Lorem Ipsum comme modèle de texte par défaut, et une recherche sur
+            "lorem ipsum" permet de découvrir de nombreux sites Web qui en sont encore à leurs débuts.
+          </p>
+
+          <a href="#conatct" className='btn btn-primary'>Découvrir nous orpholinats</a>
+        </div>
+      </div>
+    
     </section>
   )
 }
