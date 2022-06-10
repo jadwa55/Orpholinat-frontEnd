bb51cc9e95f6bd576f9e6b49e191563d09cdc3c8
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-06-10 11:26:36 +0100

    migration

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 
 const express = require("express");
 const cors = require("cors"); //cors provides Express middleware to enable CORS
+const bodyParser = require("body-parser");
+// const db = require("./config/migration");
 
 const app = express();
+
 const dotenv = require('dotenv').config();
 var corsOptions = {
   origin: "http://localhost:8080"
 };
 
 
+
+
 //* the will let us get data the data form post
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -21,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to ORPHOME" });
   });
+
 
 
 
