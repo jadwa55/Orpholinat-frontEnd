68aae82173373ae725221323b58074ff96bfce5b
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-06-11 18:33:59 +0100

    Orpholinats Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/HomePage/nav/Navbar';
 import Header from './components/HomePage/header/Header';
 import About from './components/HomePage/about/About';
@@ -9,22 +14,33 @@ import About from './components/HomePage/about/About';
 import Contact from './components/HomePage/contact/Contact';
 import Portfolio from './components/HomePage/portfolio/Portfolio';
 // import Footer from './components/footer/Footer';
+import Orpholinat from './components/Orpholinat/orpholinat';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Navbar />
-      <About />
-      {/* <Experience /> */}
-      {/* <Services /> */}
-      <Portfolio />
-      {/* <Testimonials /> */}
-      <Contact />
-      {/* <Footer /> */}
+    // <div className="App">
+    //   <Header />
+    //   <Navbar />
+    //   <About />
+    //   {/* <Experience /> */}
+    //   {/* <Services /> */}
+    //   <Portfolio />
+    //   {/* <Testimonials /> */}
+    //   <Contact />
+    //   {/* <Footer /> */}
 
 
+    // </div>
+    <div className="root">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+      <Routes>
+      <Route path="/" element={(<Orpholinat />)} />
+      </Routes>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </div>
+
+
   );
 }
 
--- frontend/src/components/Orpholinat/orpholinat.js
+++ frontend/src/components/Orpholinat/orpholinat.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./orpholinat.css";
 import {
-  getAllOr,
-  getcategorydetails,
+  getAllOrpholinat,
+  getOrpholinat,
+  createOrpholinat,
+  updateOrpholinat,
+  deleteOrpholinat
 } from "./../../../src/services/OrpholinatService";
-import { getproduct } from "./../../../src/services/ProductService";
-import { Modal, button } from "react-bootstrap";
+import { handelCatchInAxios } from "../../services/AxiosCatchService";
 
-class Category extends React.Component {
+class Orpholinat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      infocategory: this.infoCategory(),
+      infoorpholinat: this.infoOrpholinat(),
       panding: true,
       infouproducts: {},
       pandingproducts: true,
@@ -24,71 +26,59 @@ class Category extends React.Component {
     this.setState({ show: !this.state.show });
     this.setState({ fullscreen: !this.state.fullscreen });
   };
-  async infoCategory() {
+  async infoOrpholinat() {
     try {
-      let res = await getAllcategory(); // get axios promise
+      let res = await getAllOrpholinat(); // get axios promise
       let data = res.data;
-      console.log("ALL Categorys :", data);
+      console.log("ALL orpholinats :", data);
       // get all data from pomise
-      this.setState({ infocategory: data }); // Set data to state
+      this.setState({ infoOrpholinat: data }); // Set data to state
       this.setState({ panding: false }); // Change status panding for render data
     } catch (e) {
       console.error(e);
     }
   }
 
-  async detailsButton(id) {
-    try {
-      let detail = await getcategorydetails(id);
-      let data = detail.data;
-      console.log("info category products:", data);
-      this.setState({ infoproducts: data });
-      this.setState({ pandingproducts: false });
-      //   this.setState({pandingcategory: false });
-    } catch (e) {
+  async editButton (id) {
+    try{
+  
+      let res = await  getOrpholinat(id); // get axios promise
+      let data = res.data;
+      console.log("Orpholinats :", data);
+      this.setState({ infopdate: data });
+      this.setState({pandingupdate: false });
+    
+    }catch (e) {
       console.error(e);
-      //   handelCatchInAxios(e);
     }
+   
+  
   }
+  async deleteButton (id) {
+    try {
+      let deleteorpholinat = await deleteOrpholinat(id);
+      console.log("deleteButton:", deleteorpholinat);
+      alert("orpholinat is deleted!");
+      // window.location = "/dashboard/category";
+  } 
+  catch (e) {
+      console.error(e);
+      handelCatchInAxios(e);
+  }
+}
+  
 
-  async infobutonproduct(id) {
-    if (isLogin()) {
-      try {
-        let product = await getproduct(id);
-        console.log('dataproduct',product);
-        let data = JSON.parse(window.localStorage.getItem("product"));
-        let array = [data];
-        array.push(product.data);
-        console.log(product.data.id);
-        console.log(array);
-        window.localStorage.setItem("product", JSON.stringify(array));
-      } catch (e) {
-        console.error(e);
-        //   handelCatchInAxios(e);
-      }
-    } else {
-      window.location = "/login";
-    }
-  }
 
   render() {
-    // info  category:::::::::::::::::::::::::::::::::::::::::
-    let category = "";
+    // info  orpholinat:::::::::::::::::::::::::::::::::::::::::
+    let orpholinat = "";
     if (!this.state.panding) {
-      let ThisClass = this;
-      category = this.state.infocategory.map(function (category, index) {
-        let src = "http://localhost:5500/" + category.image;
+      orpholinat = this.state.infoorpholinat.map(function (orpholinat, index) {
+        let src = "http://localhost:5500/" + orpholinat.image;
         return (
+
           <div className="card box " style={{ maxWidth: "20rem" }}>
             <a href="#" className="fas fa-heart" />
-            <a
-              href="#"
-              onClick={async () => {
-                await ThisClass.detailsButton(category.id);
-                ThisClass.showModal();
-              }}
-              className="fas fa-eye"
-            />
             <img
               className="card-img-top p-3"
               src={src}
@@ -96,7 +86,7 @@ class Category extends React.Component {
               alt="Card image cap"
             />
             <div className="card-body">
-              <h3 className="card-title">{category.name}</h3>
+              <h3 className="card-title">{orpholinat.name}</h3>
               <div className="stars">
                 <i className="fas fa-star" />
                 <i className="fas fa-star" />
@@ -104,93 +94,12 @@ class Category extends React.Component {
                 <i className="fas fa-star" />
                 <i className="fas fa-star-half-alt" />
               </div>
-              <button
-                className="btnn"
-                onClick={async () => {
-                  await ThisClass.detailsButton(category.id);
-                  ThisClass.showModal();
-                }}
-              >
-                view All Product
-              </button>
             </div>
           </div>
         );
       });
     }
-    //  info products :::::::::::::::::::::::::::::::::::::::::::::
-
-    let infoproducts = "";
-    if (!this.state.pandingproducts) {
-      let ThisClass = this;
-      infoproducts = this.state.infoproducts.products.map(function (product) {
-        let src = "http://localhost:5500/" + product.image;
-
-        return (
-          <div className="card box " style={{ maxWidth: "24rem" }}>
-            <div className="image">
-              <img
-                className="card-img-top p-3"
-                src={src}
-                height={"150px"}
-                alt="Card image cap"
-              />
-
-              <a href="#" className="fas fa-heart" />
-            </div>
-
-            <div className="card-body">
-              <div className="content">
-                <div className="stars">
-                  <i className="fas fa-star" />
-                  <i className="fas fa-star" />
-                  <i className="fas fa-star" />
-                  <i className="fas fa-star" />
-                  <i className="fas fa-star-half-alt" />
-                </div>
-                <h3>{product.name}</h3>
-                <p>{product.decsription}.</p>
-                <a
-                  href="#"
-                  className="btnn"
-                  onClick={() => ThisClass.infobutonproduct(product.id)}
-                >
-                  add to cart
-                </a>
-                <span className="price">$ {product.price}</span>
-              </div>
-            </div>
-          </div>
-        );
-      });
-    }
-
-    return (
-      <div className="landingpage">
-        <Navbar />
-        <section className="dishes mt-2" id="dishes">
-          <h3 className="sub-heading mt-5"> our Category </h3>
-          <h1 className="heading"> popular category </h1>
-          <div className="box-container">{category}</div>
-        </section>
-        <Modal
-          className="modal-container custom-map-modal bg-secondary"
-          show={this.state.show}
-          fullscreen={this.state.fullscreen}
-          onHide={() => this.showModal()}
-        >
-          <Modal.Header closeButton>Products</Modal.Header>
-          <Modal.Body>
-            <section className="menu" id="menu">
-              <h3 className="sub-heading"> our menu </h3>
-              <h1 className="heading"> today's speciality </h1>
-              <div className="box-container">{infoproducts}</div>
-            </section>
-          </Modal.Body>
-        </Modal>
-      </div>
-    );
   }
 }
 
-export default Category;
+export default Orpholinat;
